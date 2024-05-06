77d3e74b4e1efcaf95bab4c2105b31fa9aae5604
CEM TASKIN <cemtaskin82@gmail.com>
2024-05-06 14:47:38 +0300

    Add new item screen will be added

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,31 +9,7 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
-    @IBOutlet weak var txtNewItem : UITextField!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        txtNewItem.becomeFirstResponder()
-    }
-    
-    @IBAction func cancel  (){
-        dismiss(animated: true)
-    }
-    
-    @IBAction func done (){
-        if let newItemText = txtNewItem.text {
-            print (newItemText)
-        }
-        
-        
-        
-        dismiss(animated: true)
-    }
 
 
 }
